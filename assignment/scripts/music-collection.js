a02fd1e4cbd3368af2cb7c9f5d02c6437d8b8478
Tim Dugan <tadugan@me.com>
2021-06-01 15:18:29 -0500

    created showCollection function, tested showCollectionFunction

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -9,18 +9,29 @@ function addToCollection(title, artist, yearPublished) {
     title: title,
     artist: artist,
     yearPublished: yearPublished
-  } // end of object declarations
+  } // end of object creation
   collection.push(record);
   return record;
 } // end addToCollection
+
+function showCollection(array) {
+  console.log('In showCollection');
+  console.log(`There are ${array.length} albums in this collection`);
+  for (let eachAlbum of array) {
+    console.log(`${eachAlbum.title} by ${eachAlbum.artist}, published in ${eachAlbum.yearPublished}`);
+  } // end of for loop
+} // end of showCollection function
 
 //testing addToCollection
 console.log(addToCollection('White Blood Cells', 'The White Stripes', 1999));
 console.log(addToCollection('Icky Thump', 'The White Stripes', 2007));
 console.log(addToCollection('Oh No', 'OK GO', 2005));
 console.log(addToCollection('Led Zeppelin IV', 'Led Zeppelin', 1971));
-console.log(addToCollection('There Is Nothing Left TO Lose', 'Foo Fighters', 1999));
+console.log(addToCollection('There Is Nothing Left To Lose', 'Foo Fighters', 1999));
 console.log(addToCollection('Pinkerton', 'Weezer', 1996));
 
 // logging whole collection
 console.log(collection);
+
+// test showCollection function
+showCollection(collection);
