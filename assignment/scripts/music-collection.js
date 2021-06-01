e390acf8379b4ace9da89b9582047d7ef02e7cfe
Tim Dugan <tadugan@me.com>
2021-06-01 15:01:09 -0500

    created addToCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,3 +1,18 @@
 console.log('***** Music Collection *****')
 
+// this variable stores the record objects we will create
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  console.log('in addToCollection');
+  let record = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  } // end of object declarations
+  collection.push(record);
+  return record;
+} // end addToCollection
+
+//testing addToCollection
+addToCollection('White Blood Cells', 'The White Stripes', 1999)
